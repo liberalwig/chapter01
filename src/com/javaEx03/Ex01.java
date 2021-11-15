706e11d5ac0a3c255e0b6dd50ab857ca5164590b
최승은 <liberalfilm@gmail.com>
2021-11-15 17:11:20 +0900

    2021.11.15(월)17:11

---
+++ src/com/javaEx03/Ex01.java
@@ -0,0 +1,22 @@
+package com.javaEx03;
+
+import java.util.Scanner;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("점수:");
+		int num = sc.nextInt();
+		
+		if(num>=60) {System.out.println("합격입니다.");} //입력한 값이 60점 "이상"이니? 괄호는 대답이 true일 때만 실행되는 영역
+		else {System.out.println("불합격입니다.");}
+		
+		System.out.println("프로그램 종료");
+		sc.close();
+	}
+	
+}
---
+++ src/com/javaEx03/Ex02.java
@@ -0,0 +1,24 @@
+package com.javaEx03;
+
+import java.util.Scanner;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("점수:");
+		int num = sc.nextInt();
+		
+		if (num>=60) {
+			System.out.println("합격입니다.");
+		}
+		
+		sc.close();
+	}
+
+}
+
+
