e44523cd3ada26131761e5fe8765fa7b4109ae8f
최승은 <liberalfilm@gmail.com>
2021-11-16 11:09:34 +0900

    2021.11.16(화)11:09AM &&와 중복시 삭제case

--- src/com/javaEx03/Ex07.java
+++ src/com/javaEx03/Ex07.java
@@ -17,7 +17,7 @@ public class Ex07 {
 		//수업 중 선생님 교정: java에서는 한 식에 두 연산자 동시 기입이 안된다. 그래서 첫 질문에 논리연산자를 써보자.
 		//나처럼 '이상'만 만족해도 되지만 두 조건 모두 만족해야 하므로 원칙적으론 동시에 만족하는 &&을 썼어도 좋음
 		if (num>=90) {System.out.println("A등급");}
-		else if (90>num && num>=80){System.out.println("B등급");} /*(num>=80)도 됐지만 새 방법*/
+		else if (90>num && num>=80){System.out.println("B등급");} /*(num>=80)도 됐지만 새 방법.다만 중복이니까 삭제 가능*/
 		//2021.11.15(월)18:14실습 중, 89>=num>=80 이렇게는 오류난다. 2021.11.16(화)10:58AM수업 중, T&&T로 가보자
 		else if (80>num && num>=70) System.out.println("C등급");
 		else if (70>num && num>=60) System.out.println("D등급");
