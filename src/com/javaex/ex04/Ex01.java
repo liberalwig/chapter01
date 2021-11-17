e01645f163d8ced8abdb445a60dca9ecbafc1731
최승은 <liberalfilm@gmail.com>
2021-11-17 10:50:34 +0900

    2021.11.17(수)10:50

--- src/com/javaex/ex04/Ex01.java
+++ src/com/javaex/ex04/Ex01.java
@@ -1,10 +1,30 @@
-package com.javaex.ex04;
+package com.javaex.ex04;//2021.11.17(수)10:35 2교시, 10.반복문 중 while반복문
+						//"while을 count세는 손에 잡는 기계 개념으로 이해해보세요"
+						//그런데 tail변수를 잡고보니 똑같은 일을 하는 변수i가 있음을 발견=>tail없애고 해보기
 
 public class Ex01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int i = 0;
+		
+		while (i<5) {
+			System.out.println("I Like Java" + (i+3));
+			i++;
+		}
+		
+		/*int tail = 0;
+		int i = 3;
+		
+		while(i<8) {
+			System.out.println("I Like Java" + i);
+			i++;
+			//tail++; 
+		*/
+		
+		
+		
 	}
 
 }
